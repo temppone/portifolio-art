5f16a2dd8dd14760c4422a4d92957d9be6db5b9b
gustavo <gtvtempone@gmail.com>
2021-06-14 16:14:12 -0300

    Initial commit

---
+++ src/Componenents/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HeaderPage } from './Header.styled'
+
+
+const Header = () => {
+  return (
+    <HeaderPage>
+      
+    </HeaderPage>
+  )
+}
+
+export default Header
