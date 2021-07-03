e86e36024f0e18d7f7c618b28251e52d0a396ad1
gustavo <gtvtempone@gmail.com>
2021-07-03 17:33:57 -0300

    feat: animations on menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { MenuContainer } from "./Menu.styled";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MenuContainer, SquareMenu } from './Menu.styled';
+
 
 const Menu = ({ openMenu }) => {
   return (
     <MenuContainer openMenu={openMenu}>
-      <Link href="/">Início</Link>
-      <Link href="/">Sobre</Link>
-      <Link href="/">Contato</Link>
+      <Link href="/">
+        <SquareMenu /> Início
+      </Link>
+      <Link href="/">
+        <SquareMenu /> Sobre
+      </Link>
+      <Link href="/">
+        <SquareMenu /> Contato
+      </Link>
     </MenuContainer>
   );
 };
--- src/Components/Menu/Menu.styled.js
+++ src/Components/Menu/Menu.styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const MenuContainer = styled.div`
   display: flex;
@@ -9,23 +9,62 @@ export const MenuContainer = styled.div`
   width: 100%;
   height: 100vh;
   text-align: center;
-  transition: transform 0.1s ease-in-out;
   left: 0;
   top: 0;
   z-index: 2;
+
+  transition: opacity 0.35s cubic-bezier(0.55, 0, 1, 0.45),
+    visibility 0.35s cubic-bezier(0.55, 0, 1, 0.45),
+    transform 0.35s cubic-bezier(0.55, 0, 1, 0.45);
+
   transform: ${({ openMenu }) =>
-    openMenu ? "translateX(0)" : "translateX(-100%)"};
+    openMenu ? 'translateY(0)' : 'translateY(-100%)'};
 
   a {
     color: ${({ theme }) => theme.primaryLight};
     transition: transform 0.3 linear;
     font-size: 2rem;
     padding: 2rem 1rem;
+    transition: all 0.8s linear;
+    width: 100%;
+    margin: 0 auto;
+
+    opacity: ${({ openMenu }) => (openMenu ? '1' : '0')};
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.xs}) {
+      font-size: 3rem;
+    }
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+      font-size: 3.5rem;
+    }
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+      font-size: 4rem;
+    }
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+      font-size: 4.5rem;
+    }
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.xl}) {
+      font-size: 5rem;
+    }
+
+    :hover,
+    :hover div {
+      transform: scale(1.1);
+      transition: all 0.3s linear;
+    }
   }
+`;
 
-
-
-
+export const SquareMenu = styled.div`
+  width: 0.5rem;
+  height: 0.5rem;
+  background: ${({ theme }) => theme.primaryLight};
+  display: inline-block;
+  transform: translateY(-60%);
 `;
 
 export const MenuLink = styled.a``;
